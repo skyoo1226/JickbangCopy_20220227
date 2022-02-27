b50d7b2ea62380b0099640bf2b54a304acfedca6
skyoo1226 <you1226@naver.com>
2022-02-27 14:23:28 +0900

    새로운 패키지에 새로운 클래스를 만들어 코딩

---
+++ app/src/main/java/com/knta/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -0,0 +1,30 @@
+package com.knta.jickbangcopy_20220227.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.knta.jickbangcopy_20220227.R
+import com.knta.jickbangcopy_20220227.datas.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: ArrayList<RoomData>
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {  // Ctrl + P => 지원 생성자 목록 조회
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow =  convertView
+
+        if (tempRow == null) {
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
---
+++ app/src/main/java/com/knta/jickbangcopy_20220227/datas/RoomData.kt
@@ -0,0 +1,11 @@
+package com.knta.jickbangcopy_20220227.datas
+
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String,
+) {
+
+
+}
