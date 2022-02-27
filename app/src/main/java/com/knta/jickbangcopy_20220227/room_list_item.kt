8abff5951d705783ce0ba03c5e631709086b3b2e
skyoo1226 <you1226@naver.com>
2022-02-27 13:59:54 +0900

    UI 작업 첫번째

---
+++ app/src/main/java/com/knta/jickbangcopy_20220227/room_list_item.kt
@@ -0,0 +1,11 @@
+package com.knta.jickbangcopy_20220227
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class room_list_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_list_item)
+    }
+}
